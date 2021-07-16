9e5dc258f4ba741a9d554db8efe07ce86147950d
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-07-16 10:48:45 +0530

    dsa

--- StartingOfDS/Queue/ReverseFirstKelementsOfQueue.py
+++ StartingOfDS/Queue/ReverseFirstKelementsOfQueue.py
@@ -1,11 +1,30 @@
+##import queue
+##def reverseFirst(q,k):
+##    s=[]
+##    while(i<k):
+##        s.append(queue.queue[0])
+##        queue.get()
+##    while(len(Stack) != 0):  
+##        queue.put(s[-1])  
+##        s.pop() 
+##        
+##    
 import queue
-def reverseFirst(q,k):
-    s=[]
-    while(i<k):
-        s.append(queue.queue[0])
-        queue.get()
-    while(len(Stack) != 0):  
-        queue.put(s[-1])  
-        s.pop() 
-        
+def rev(arr, k):
+    q = queue.Queue()
+    for ele in arr:
+        q.put(ele)
     
+    s = []
+    for i in range(k):
+        s.append(q.get())
+    while len(s) != 0:
+        q.put(s.pop())
+    for i in range(q.qsize() - k):
+        q.put(q.get())
+    while q.empty() is not True:
+        print(q.get())
+    
+arr = [10,20,30,40,50,60,70,80,90]
+k = 4
+rev(arr, k)
