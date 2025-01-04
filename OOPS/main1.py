b7cecee08002c5d2513590877f7afcbe2764522b
nilank nikhil <nilanknikhil@gmail.com>
2025-01-05 04:15:38 +0530

    revised  class attributes and how to play at class level and instance level

--- OOPS/main1.py
+++ OOPS/main1.py
@@ -15,15 +15,31 @@ class Item:
     def calculate_total_price(self):
         return self.price * self.quantity
     
+    def apply_discount(self):
+        # self.price = self.price * Item.pay_rate
+        # if we use Item.pay_rate, it will always use the class attribute, but if we use self.pay_rate, it will first look for the attribute on the instance itself, if it is not found, it will go ahead and look for the class attribute.
+        self.price = self.price * self.pay_rate
 
-item1 = Item(1, 100, 5)
+        
+# item1 = Item(1, 100, 5)
+# item2 = Item("Laptop", 1000, 3)
+
+# print(Item.pay_rate)
+# print(item1.pay_rate)
+# print(item2.pay_rate)
+# # so for each instance, first it willl try to find the pay_rate on the instance itself, if it is not found, it will go ahead and look for the class attribute.
+
+
+# print(Item.__dict__) # it will show all the class attributes
+# print(item1.__dict__) # it will show all the instance attributes
+
+
+item1 = Item("Phone", 100, 5)
+item1.apply_discount()
+print(item1.price)
+
+# suppose for second item, i want to change the pay_rate
 item2 = Item("Laptop", 1000, 3)
-
-print(Item.pay_rate)
-print(item1.pay_rate)
-print(item2.pay_rate)
-# so for each instance, first it willl try to find the pay_rate on the instance itself, if it is not found, it will go ahead and look for the class attribute.
-
-
-print(Item.__dict__) # it will show all the class attributes
-print(item1.__dict__) # it will show all the instance attributes
+item2.pay_rate = 0.7
+item2.apply_discount()
+print(item2.price)
